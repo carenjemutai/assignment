8f0f2487d880a90e0dfb50e038345f7ec543cabd
carenjemutai <carenjemutai3276@gmail.com>
2025-03-10 21:26:56 +0300

    Add files via upload

---
+++ Do-while loop 1.cpp
@@ -0,0 +1,22 @@
+/*
+do-while loops
+Author:Caren Kiptoo
+Reg no:D33-2447-2023
+Date:10-3-2025
+
+*/
+#include <stdio.h>
+
+int main() {
+	int i=1; // start 
+	do{
+	printf("%d \n", i*i*i);
+	i++; 
+	
+	
+	
+	}while(i<=5);
+	
+	return 0; // successful execution
+}
+	
