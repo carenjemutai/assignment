d7057542b7d97b10c058b9ce5b2b69064ea38f43
carenjemutai <carenjemutai3276@gmail.com>
2025-03-10 21:30:09 +0300

    Add files via upload

---
+++ whileloop 1.cpp
@@ -0,0 +1,20 @@
+/*
+While loops
+Author:Caren Kiptoo
+Reg No:D33-2447-2023
+Date:10-3-2025
+*/
+#include <stdio.h>
+
+int main() {
+	int n, i=1; //start
+	while(i<=5) { // stop
+		printf("%d \n", i*i*i);
+	i++;
+	}
+	
+	return 0;
+	
+}
+	
+	
