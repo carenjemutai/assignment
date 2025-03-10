013a95176b63ab0216e2f2c3c146ef121c90659d
carenjemutai <carenjemutai3276@gmail.com>
2025-03-10 21:29:04 +0300

    Add files via upload

---
+++ for loop 1.cpp
@@ -0,0 +1,18 @@
+/*
+For loop
+Author:Caren Kiptoo
+Reg no:D33-2447-2023
+Date:10-3-2025
+*/
+
+#include <stdio.h>
+
+int main(){
+	int i;
+	for(i=1; i<=5; i++) {
+		printf("%d \n", i*i*i);
+	}
+	
+	return 0; // successful execution
+	
+}
